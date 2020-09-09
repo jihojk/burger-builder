9552fd5304a7051f074b7e5d42cd574df5a93bb9
Jiho Kim <jihojk@uci.edu>
2020-09-09 03:50:48 -0700

    control  buttons components built

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,10 +4,11 @@ import classes from './BuildControl.css';
 
 
 const buildControl = (props) => (
-    <div className={classes.buildControl}>
+    <div className={classes.BuildControl}>
         <div className={classes.Label}> {props.label}</div>
-        <button  className={classes.Remove}> Remove </button>
         <button className={classes.Add}> Add </button>
+        <button  className={classes.Remove}> Remove </button>
+        
     </div>
 
 
