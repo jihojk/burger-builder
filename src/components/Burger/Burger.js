3c66e75d822e1e84711ced1a660280588f533612
Jiho Kim <jihojk@uci.edu>
2020-09-09 03:27:08 -0700

    builder control buttons style and components

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import classes from './Burger.css';
 import  BurgerIngredients from './BurgerIngredients/BurgerIngredients';
@@ -6,13 +6,19 @@ import  BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey=>{
             return [...Array(props.ingredients[igKey] )].map((_, i)=>{
                return <BurgerIngredients key={igKey + i} type={igKey} />
             } );
-        });
-    ;
+        }).reduce((arr, el)=>{
+            return arr.concat(el)
+        },[]);
+        if(transformedIngredients.length === 0){
+            transformedIngredients = <p> Start adding ingredients! </p>
+        }
+        console.log(transformedIngredients);
+    
     return (
         <div className={classes.Burger}> 
         <BurgerIngredients type ="bread-top" />
