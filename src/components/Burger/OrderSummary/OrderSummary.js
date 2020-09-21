4cbfa3f58af8843a0f94bd8ca41bc7f734227eb0
Jiho Kim <jihojk@uci.edu>
2020-09-21 07:14:49 -0700

    updated readme

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,6 +4,9 @@ import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
+    componentWillUpdate(){
+        console.log('[OrderSummary] WillUpdate');
+    }
 render (){
     const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
